46bd3baa128120d578f5a9266becea34581ce99f
Adhoc <pman273@gmail.com>
2017-03-22 20:24:24 -0400

    woot released

---
+++ woot/woot.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+
+class woot:
+    """woot: cheers when a user types \o/"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def \o/(self):
+        """This does stuff!"""
+
+        #Your code will go here
+        await self.bot.say("Huzzah! \o/")
+
+def setup(bot):
+    bot.add_cog(woot(bot))
